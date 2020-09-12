d8f8b8bbc5b731fccdab349ec6de0e299aec5b01
lauraramirezencinas <lau.ramirez.encinas@gmail.com>
2020-09-12 11:14:00 +0200

    fix

--- src/components/bakerys/HojaPedidoBaker.js
+++ src/components/bakerys/HojaPedidoBaker.js
@@ -8,30 +8,84 @@ export class HojaPedidoBaker extends Component {
         super(props)
         this.state = {
             pedidos: [],
+            filtroEstado: null,
+            filtro: false
 
         }
     }
 
+    handleChange = async (event) => {
+        const { name, value } = event.target;
+        await this.setState({
+            [name]: value,
+            filtro: true
+        });
+        this.actualizarPedidos()
+    };
+
+    actualizarPedidos() {
+        if(this.state.filtro){
+            axios.get('http://localhost:3000/pedido/all/?status=' + this.state.filtroEstado + "&time=" + new Date().valueOf(), 
+            { withCredentials: true })
+                .then(response => {
+                    console.log("pedidos", response.data)
+                    this.setState({ pedidos: response.data })
+
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }else{
+            axios.get('http://localhost:3000/pedido/all/' + "?time=" + new Date().valueOf(), { withCredentials: true })
+                .then(response => {
+                    console.log("pedidos", response.data)
+                    this.setState({ pedidos: response.data })
+
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+    }
+
+   
+
     componentDidMount() {
-        axios.get('http://localhost:3000/pedido/all/'+ "?time=" + new Date().valueOf(), { withCredentials: true })
-            .then(response => {
-                console.log("pedidos", response.data)
-                this.setState({ pedidos: response.data })
-                
-            })
-            .catch(err=>{
-                console.log(err)
-            })
+        this.actualizarPedidos()
     }
 
 
     render() {
-        const pedidosTodos = this.state.pedidos.map((pedido) => 
-            <PedidoBaker key={pedido._id}  pedido={pedido}/>)
-    
+        const pedidosTodos = this.state.pedidos.map((pedido) =>
+            <PedidoBaker key={pedido._id} pedido={pedido} />)
+
 
         return (
-            <div >
+            <div className="" >
+                <div className="container">
+                    <form>
+                        <div class="form-group ">
+                            <div className="row mt-30">
+                                <div className="col">
+                                    <label class="mr-sm-2">Filtrar por estado:</label>
+                                </div>
+                                <div className="col-5 col-md-4 col-lg-4">
+                                    <select class="custom-select sel-status" name="filtroEstado"
+                                        value={this.state.filtroEstado}
+                                        onChange={(e) => this.handleChange(e)} >
+                                        <option>Selecionar..</option>
+                                        <option value="Nuevo">Nuevo</option>
+                                        <option value="EnPreparacion">En preparacion</option>
+                                        <option value="Finalizado">Finalizado</option>
+                                        <option value="Entregado">Entregado</option>
+                                        <option value="Cancelado">Cancelado</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
                 {pedidosTodos}
             </div>
 
--- src/components/bakerys/PedidoBaker.js
+++ src/components/bakerys/PedidoBaker.js
@@ -12,30 +12,30 @@ export class Pedido extends Component {
     }
 
     handleChange = async (event) => {
-       await this.setState({status: event.target.value})
-       this. handleFormSubmit(event)
+        await this.setState({ status: event.target.value })
+        this.handleFormSubmit(event)
     };
 
 
-    handleFormSubmit =  async (event) => {
+    handleFormSubmit = async (event) => {
         event.preventDefault();
         const status = this.state.status
-          axios.patch(`http://localhost:3000/pedido/${this.props.pedido._id}`,
+        axios.patch(`http://localhost:3000/pedido/${this.props.pedido._id}`,
             { status: status }, { withCredentials: true })
             .then(res => {
                 console.log(res.data)
-                let url = "/pedidos/" 
+                let url = "/pedidos/"
                 window.location.href = url;
             })
     }
 
     render() {
         const listaStatus = [
-            { name:"Nuevo", value: "Nuevo" },
-            { name:"Preparacion", value: "EnPreparacion" },
-            { name:"Finalizado", value: "Finalizado" },
-            { name:"Entregado", value: "Entregado" },
-            { name:"Cancelado", value: "Cancelado" }
+            { name: "Nuevo", value: "Nuevo" },
+            { name: "Preparacion", value: "EnPreparacion" },
+            { name: "Finalizado", value: "Finalizado" },
+            { name: "Entregado", value: "Entregado" },
+            { name: "Cancelado", value: "Cancelado" }
         ]
 
         const pintarLista = listaStatus.map((status, idx) =>
@@ -48,28 +48,45 @@ export class Pedido extends Component {
                 value={status.value}
                 checked={this.state.status === status.value}
                 onChange={(e) => this.handleChange(e)}
-                >
+            >
                 {status.name}
             </ToggleButton>
         )
 
-        
-        const detallePedido= this.props.pedido.items.map(infoPedido=>
-           <Item infoPedido={infoPedido} />)
+
+        const detallePedido = this.props.pedido.items.map(infoPedido =>
+            <Item key={infoPedido._id} infoPedido={infoPedido} />)
 
         return (
             <div className="pedido-baker">
-            <div className="container  mb-50 mt-30">
-                <p className="numero-pedido">Pedido Numero {this.props.pedido.numeroPedido} </p>
-                <p className="detalles">Detalles del pedido:</p>
-                 {detallePedido} 
-                 <p className="precio-total">Total:{this.props.pedido.precioTotal}€</p>
-                 <p> Estado:{this.state.status}</p>
-                <ButtonGroup vertical toggle>                
-                {pintarLista}
-                </ButtonGroup>
-        
-            </div>
+                <div className="container  mb-50 mt-30">
+                    <div className="row">
+                        <div className="col">
+                            <p className="numero-pedido">Pedido Numero {this.props.pedido.numeroPedido} </p>
+                        </div>
+                        <div className="col">
+                            <p className="fecha">{this.props.pedido.created_at} </p>
+                        </div>
+                    </div>
+
+                    <div className=" row">
+                        <div className="col">
+                            <p className="datos">Nombre: {this.props.pedido.nombre} </p>
+                        </div>
+                        <div className="col">
+                            <p className="datos">Telefono: {this.props.pedido.telefono}</p>
+                        </div>
+                    </div>
+                    <p className="fecha">Entregar en:  {this.props.pedido.recogida} </p>
+                    <p className="detalles">Detalles del pedido:</p>
+                    {detallePedido}
+                    <p className="precio-total">Total:{this.props.pedido.precioTotal}€</p>
+                    <p> Estado:{this.state.status}</p>
+                    <ButtonGroup vertical toggle>
+                        {pintarLista}
+                    </ButtonGroup>
+
+                </div>
             </div>
         )
     }
